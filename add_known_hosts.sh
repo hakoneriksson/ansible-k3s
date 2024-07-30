314a14cca5184aa0e5feebeba54255bf86b19fc5
Håkon Eriksson <38030236+hakoneriksson@users.noreply.github.com>
2024-07-30 19:41:31 +0200

    from inventory

--- add_known_hosts.sh
+++ add_known_hosts.sh
@@ -1,4 +1,4 @@
 # Add all ansible inventory hosts to the known hosts file  ~/.ssh/known_hosts using ssh-keyscan
-for host in $(cat /etc/ansible/hosts | grep -v "\[" | grep -v "^$"); do
+for host in $(cat inventory/hosts | grep -v "\[" | grep -v "^$"); do
     ssh-keyscan -H $host >> ~/.ssh/known_hosts
 done
